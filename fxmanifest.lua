ea41ae0eeca35436db49cd882ab5204e385e2c52
Keys <49612041+Ceilo@users.noreply.github.com>
2023-05-27 23:49:51 -0400

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,25 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Your Name'
+description 'Cat Cafe Resource'
+version '1.0.0'
+
+client_scripts {
+    'client.lua'
+}
+
+server_scripts {
+    'server.lua'
+}
+
+files {
+    'plushie_model_name_1.ydr',
+    'plushie_model_name_2.ydr',
+    'plushie_model_name_3.ydr',
+    -- Add any additional model files here
+}
+
+dependencies {
+    'qb-core' -- Replace with the name of your QB Core dependency
+}
