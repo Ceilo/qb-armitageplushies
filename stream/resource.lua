8e39d9cd597969c0044adb9b1afdf2d53e56b35a
Keys <49612041+Ceilo@users.noreply.github.com>
2023-05-27 23:37:11 -0400

    Create resource.lua

---
+++ stream/resource.lua
@@ -0,0 +1,17 @@
+-- Resource Metadata
+resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+
+client_scripts {
+    'client.lua',
+}
+
+server_scripts {
+    'server.lua',
+}
+
+files {
+    'stream/cat_cafe.ydr',
+}
+
+data_file 'DLC_ITYP_REQUEST' 'stream/cat_cafe.ytyp'
+
